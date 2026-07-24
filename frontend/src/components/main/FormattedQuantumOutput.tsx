e47ef085a5635be8e24f2d8cf8c481797f6d95a2
Andrei-Carabiber <andicarabiber@gmail.com>
2026-07-24 14:24:31 +0200

    Added recharts and fixed logging in server

--- frontend/src/components/main/FormattedQuantumOutput.tsx
+++ frontend/src/components/main/FormattedQuantumOutput.tsx
@@ -1,40 +1,11 @@
-const FormattedQuantumOutput = ({data} : {data: string}) => {
-    const dataObj = JSON.parse(data)
 
-    // @ts-ignore
-    function extractTypes(data:any) {
-        // Handle null values (typeof null is 'object' in JS, which is a known quirk)
-        if (data === null) return 'null';
 
-        // Handle Arrays
-        if (Array.isArray(data)) {
-            if (data.length === 0) return 'any[]';
-            // Grab the type of the first element in the array
-            return `${extractTypes(data[0])}[]`;
-        }
+const FormattedQuantumOutput = ({ data }: { data: string }) => {
+    const dataObj = JSON.parse(data);
 
-        // Handle Objects recursively
-        if (typeof data === 'object') {
-            const schema = {};
-            for (const key in data) {
-                // @ts-ignore
-                schema[key] = extractTypes(data[key]);
-            }
-            return schema;
-        }
-
-        // Handle Primitives (strings, numbers, booleans)
-        return typeof data;
-    }
-
-    console.log(extractTypes(dataObj));
-
+    console.log(dataObj)
     return (
-        <div>
-            {data}
-
-            \\
-        </div>
+        data
     );
 };
 
